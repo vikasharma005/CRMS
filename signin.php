6f38786fb207f4839d0c8d9fc4e01cb3b42aada8
Vikas Sharma <93978146+vikasharma005@users.noreply.github.com>
2022-11-20 10:20:55 +0530

    Update signin.php

--- signin.php
+++ signin.php
@@ -1,48 +1,11 @@
-<?php
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
 
-$servername ="localhost";
-$username = "root";
-$password = "";
-$dbname = "codeflix";
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if($conn->connect_error){
-	die("connection failed");
-}
-
-$email = $_POST["email"];
-$password = $_POST["password"];
-$salt = "codeflix";
-$password_encrypted = sha1($password.$salt);
-
-
-$sql = mysqli_query($conn, "SELECT count(*) as total from signup WHERE email = '".$email."' and 
-	password = '".$password_encrypted."'");
-
-$row = mysqli_fetch_array($sql);
-
-if($row["total"] > 0){
-	?>
-	<script>
-		alert('Login successful');
-	</script>
-	
-	<?php
-}
-else{
-	?>
-	<script>
-		alert('Login failed');
-	</script>
-	<?php
-}
-
-
-
-
-
-
-
-
-?>
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
